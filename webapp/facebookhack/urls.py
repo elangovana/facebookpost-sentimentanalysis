537d9293f726bc0c4981d88252bc8e0e7cb4cf06
elangovana <aparnaelangovan@gmail.com>
2017-05-06 21:03:10 +1000

    get page to work with get and post

--- webapp/facebookhack/urls.py
+++ webapp/facebookhack/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^proxy/', include('proxy.urls')),
+
     url(r'^admin/', admin.site.urls),
+    url(r'^.*$', include('proxy.urls')),
 ]
--- webapp/proxy/views.py
+++ webapp/proxy/views.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import logging
 import urllib
 import re
 
+
+
 import sys
 import urllib2
+import pdb
 
 from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -18,15 +23,45 @@ sys.setdefaultencoding('utf8')
 
 def transform_reponse(html):
     result = html
-    result = re.sub(r"\"https://[^\"]*facebook.com", "http://localhost/proxy", html)
+    #TODO: fix path issue
+    result = re.sub(r"\"https://[^\"]*facebook.com", "http://localhost:8000/proxy", result)
     return result
 
+def transform_request(html):
+    result = html
+    # TODO: fix path issue
+    result = re.sub(r"\"http://localhost:8000/proxy", "https://www.facebook.com",  result)
+    return result
 
+@csrf_exempt
 def index(request):
     # Accept-Language: da, en-gb
-    req = urllib2.Request('https://www.facebook.com')
-    req.add_header('Accept-Language', 'en-gb')
-    response = urllib2.urlopen(req)
-    html = response.read()
+    #TODO:fix path issue
+    fbrelativePath = re.sub(r"^/proxy/", "",request.path_info)
+
+    fbreponse = get_fbresponse(fbrelativePath, request)
+
+
+    #fbreponse cookies
+    cookies = fbreponse.info()['Set-Cookie']
+    #fbhtmlcontent
+    html = fbreponse.read()
     transformed_html = transform_reponse(html)
-    return HttpResponse(transformed_html)
+    #return
+    response = HttpResponse(transformed_html)
+    response.set_cookie( cookies)
+    return response
+
+
+def get_fbresponse(fbrelativePath, request):
+    path = 'https://www.facebook.com/' + fbrelativePath
+    fbrequest = urllib2.Request(path)
+    fbrequest.add_header('Accept-Language', 'en-gb')
+    # add cookies
+    fbrequest.add_header('Cookie', "; ".join('%s=%s' % (k, v) for k, v in request.COOKIES.items()))
+    if (request.method != 'GET'):
+        fbrequest.add_data(transform_request(request.body))
+
+    # get resposne
+    fbreponse = urllib2.urlopen(fbrequest)
+    return fbreponse
